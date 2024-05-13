484775c9464f885e1cdd1edfa3dc33a99ca1c3ef
Ju0011 <yhs07240@naver.com>
2024-05-13 11:16:26 +0900

    서버 배포 test

--- src/main/java/ju00/freeload/controller/TestController.java
+++ src/main/java/ju00/freeload/controller/TestController.java
@@ -32,27 +32,27 @@ public class TestController {
     }
 
     // /test경로는 이미 존재하므로 /test/testRequestBody로 지정했다.
-    @GetMapping("/testRequestBody")     // @RequestBody ~ : RequestBody로 보내오는 JSON을 TestReqestBodyDTO 오브젝트로 변환
-    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
-        return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
-    }
-
-    @GetMapping("/testResponseBody")    // ResponseDTO를 반환하는 컨트롤러
-    //(GET) http://localhost:8080/test/testResponseBody
-    public ResponseDTO<String> testControllerResponseBody() {
-        List<String> list = new ArrayList<>();
-        list.add("Hello World! I'm ResponseDTO");
-        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-        return response;
-    }
-
-    @GetMapping("/testResponseEntity")  // ResponseEntity를 반환하는 컨트롤러
-    //(GET) http://localhost:8080/test/testResponseEntity
-    public ResponseEntity<?> testControllerResponseEntity() {
-        List<String> list = new ArrayList<>();
-        list.add("Hello World! I'm ResponseEntity. And you got 400!");
-        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
-        // http status를 400로 설정.
-        return ResponseEntity.badRequest().body(response);
-    }
+//    @GetMapping("/testRequestBody")     // @RequestBody ~ : RequestBody로 보내오는 JSON을 TestReqestBodyDTO 오브젝트로 변환
+//    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+//        return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+//    }
+//
+//    @GetMapping("/testResponseBody")    // ResponseDTO를 반환하는 컨트롤러
+//    //(GET) http://localhost:8080/test/testResponseBody
+//    public ResponseDTO<String> testControllerResponseBody() {
+//        List<String> list = new ArrayList<>();
+//        list.add("Hello World! I'm ResponseDTO");
+//        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+//        return response;
+//    }
+//
+//    @GetMapping("/testResponseEntity")  // ResponseEntity를 반환하는 컨트롤러
+//    //(GET) http://localhost:8080/test/testResponseEntity
+//    public ResponseEntity<?> testControllerResponseEntity() {
+//        List<String> list = new ArrayList<>();
+//        list.add("Hello World! I'm ResponseEntity. And you got 400!");
+//        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+//        // http status를 400로 설정.
+//        return ResponseEntity.badRequest().body(response);
+//    }
 }
