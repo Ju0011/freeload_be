d8013d30b139f49abe63d94c6bc2e3c04331ca75
Ju0011 <yhs07240@naver.com>
2024-06-12 11:38:23 +0900

    get_rest responseBody 수정중

---
+++ src/main/java/ju00/freeload/dto/RestDTO_user.java
@@ -0,0 +1,74 @@
+package ju00.freeload.dto;
+
+import ju00.freeload.model.RestEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+//@NoArgsConstructor  // 기본 생성자 자동 추가
+@AllArgsConstructor
+@Data
+public class RestDTO_user {
+    private Long restId;      // 휴게소 아이디
+    private String restName;  // 휴게소 이름
+    private String restAddr;   // 주소
+    private String gudClssCd;   // 상, 하행 구분
+    private String routeNm;     //휴게소가 소속된 고속도로 이름
+    private String hdqrCd;      //본부코드
+    private String routeCd;     // 고속도로 코드
+    private String rprsTelNo;
+    private String wifi;
+    private String satisfaction;
+    private String electric_car;
+    private String nursing_room;
+    private String pharmacy;
+    private String Braile_block;
+    private String pet;
+    private String latitude;
+    private String longitude;
+
+    private boolean isLiked;
+
+
+    public RestDTO_user(final RestEntity entity, boolean isLiked) {
+        this.restId = entity.getSvarCd();
+        this.restName = entity.getRestNm();
+        this.restAddr = entity.getRestAddr();
+        this.routeNm = entity.getRouteNm();
+        this.gudClssCd = entity.getGudClssCd();
+        this.hdqrCd = entity.getHdqrCd();
+        this.routeCd = entity.getRouteCd();
+        this.rprsTelNo = entity.getRprsTelNo();
+        this.wifi = entity.getWifi();
+        this.satisfaction = entity.getSatisfaction();
+        this.electric_car = entity.getElectric_car();
+        this.nursing_room = entity.getNursing_room();
+        this.pharmacy = entity.getPharmacy();
+        this.Braile_block = entity.getBraile_block();
+        this.pet = entity.getPet();
+        this.latitude = entity.getLatitude();
+        this.longitude = entity.getLongitude();
+        this.isLiked = isLiked;
+    }
+
+    public static RestEntity toEntity(final RestDTO_user dto) {
+        return RestEntity.builder()
+                .svarCd(dto.getRestId())
+                .restNm(dto.getRestName())
+                .restAddr(dto.getRestAddr())
+                .routeNm(dto.getRouteNm())
+                .gudClssCd(dto.getGudClssCd())
+                .hdqrCd(dto.getHdqrCd())
+                .routeCd(dto.getRouteCd())
+                .rprsTelNo(dto.getRprsTelNo())
+                .wifi(dto.getWifi())
+                .satisfaction(dto.getSatisfaction())
+                .electric_car(dto.getElectric_car())
+                .nursing_room(dto.getNursing_room())
+                .pharmacy(dto.getPharmacy())
+                .Braile_block(dto.getBraile_block())
+                .pet(dto.getPet())
+                .latitude(dto.getLatitude())
+                .longitude(dto.getLongitude())
+                .build();
+    }
+}
